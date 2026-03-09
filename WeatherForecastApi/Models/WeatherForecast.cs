aa5f77be1e8163ef489dcae01c2c0be6dc10a33f
ngunasekar <ngunasekar@testsys.com>
2026-03-09 13:34:36 +0530

    removed whether fore casting

--- WeatherForecastApi/Models/WeatherForecast.cs
+++
@@ -1,6 +0,0 @@
-namespace WeatherForecastApi.Models;
-
-public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC * 9.0 / 5.0);
-}
